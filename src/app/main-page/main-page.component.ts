5d6b2ecd55c15073e2d0554b2226378c1199fb99
Arman <hurianarmann@gmail.com>
2023-08-02 22:59:46 +0430

    create new session in main page and add iHover Effect

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class MainPageComponent {
 
+  @ViewChild('showUserDes') 'showUserDes' : ElementRef
+  @ViewChild('clickParent') 'clickParent' : ElementRef
+  @ViewChild('categoriesList') 'categoriesList' : ElementRef
+
+  showUserDescription(){
+
+    if(this.showUserDes.nativeElement.style.opacity!== '1'){
+
+      this.showUserDes.nativeElement.style.opacity = '1'
+      this.showUserDes.nativeElement.style.visibility = 'visible'
+      this.clickParent.nativeElement.style.backgroundColor = '#f2747466'
+      this.clickParent.nativeElement.style.borderRadius = '0.4rem'
+    }else{
+      this.showUserDes.nativeElement.style.opacity = '0'
+      this.showUserDes.nativeElement.style.visibility = 'hidden'
+      this.clickParent.nativeElement.style.backgroundColor = 'white'
+      
+    }
+    
+  }
+  showCategoryList(event:any){
+    event.preventDefault()
+    if(this.categoriesList.nativeElement.style.opacity!== '1'){
+  
+      this.categoriesList.nativeElement.style.opacity = '1'
+      this.categoriesList.nativeElement.style.visibility = 'visible'
+
+    }else{
+      this.categoriesList.nativeElement.style.opacity = '0'
+      this.categoriesList.nativeElement.style.visibility = 'hidden'
+   
+    }
+    
+  }
 }
