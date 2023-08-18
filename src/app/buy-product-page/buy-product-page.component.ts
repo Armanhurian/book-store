c2d92f8cb8020cb4f142d38f21311e8fdd37ac4a
Arman <hurianarmann@gmail.com>
2023-08-19 00:08:50 +0430

    make dynamic buy product page

--- src/app/buy-product-page/buy-product-page.component.ts
+++ src/app/buy-product-page/buy-product-page.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit , ElementRef ,ViewChild, HostListener } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-buy-product-page',
   templateUrl: './buy-product-page.component.html',
   styleUrls: ['./buy-product-page.component.css']
 })
-export class BuyProductPageComponent {
+export class BuyProductPageComponent implements OnInit{
 
+
+  constructor( private productService : ProductService , private route : ActivatedRoute ){}
+
+  @ViewChild('showUserDes') 'showUserDes' : ElementRef
+  @ViewChild('clickParent') 'clickParent' : ElementRef
+  @ViewChild('categoriesList') 'categoriesList' : ElementRef
+
+  dashbordNameLists : any = []
+
+  dashbordName : any =  ''
+
+  productId ?: number | string ;
+
+  products : any[] = []
+
+  dashbordNameFunc(){
+    this.dashbordNameLists.push(localStorage.getItem('dashbordName'))
+    this.dashbordNameLists.push(localStorage.getItem('nameValueInput'))
+    this.dashbordName = this.dashbordNameLists[this.dashbordNameLists.length - 1]
+  }
+
+  logoutHandler(){
+    console.log('log out');
+    localStorage.clear()
+  }
+
+  showUserDescription(){
+
+    if(this.showUserDes.nativeElement.style.opacity!== '1'){
+
+      this.showUserDes.nativeElement.style.opacity = '1'
+      this.showUserDes.nativeElement.style.visibility = 'visible'
+      this.clickParent.nativeElement.style.backgroundColor = '#f2747466'
+      this.clickParent.nativeElement.style.borderRadius = '0.4rem'
+      
+    }else{
+      this.showUserDes.nativeElement.style.opacity = '0'
+      this.showUserDes.nativeElement.style.visibility = 'hidden'
+      this.clickParent.nativeElement.style.backgroundColor = 'white'
+      
+    }
+    
+  }
+  showCategoryList(event:any){
+    event.preventDefault()
+    if(this.categoriesList.nativeElement.style.opacity!== '1'){
+  
+      this.categoriesList.nativeElement.style.opacity = '1'
+      this.categoriesList.nativeElement.style.visibility = 'visible'
+
+    }else{
+      this.categoriesList.nativeElement.style.opacity = '0'
+      this.categoriesList.nativeElement.style.visibility = 'hidden'
+   
+    }
+    
+    
+  }
+    
+  ngOnInit(): void {
+    
+    this.productId = Number(this.route.snapshot.params['id'])
+
+    this.products = this.productService.computerProductsInMainPage.filter(item => item.id === this.productId)
+    
+  }
 }
